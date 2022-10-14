059984417182570aedb2341608a5fc96560409dd
edaccp <edaccp@gmail.com>
2022-10-13 21:32:22 -0300

    Widgets 5

--- lib/Widgets/05_stone_fragments/e05_stones.dart
+++ lib/Widgets/05_stone_fragments/e05_stones.dart
@@ -5,14 +5,20 @@ Desenvolver um aplicativo que faça a contagem dos cliques do usuário na tela e
 
 ## Instruções
 1. Crie uma aplicação em Flutter que rode a classe Stone.
-2. Ainda no arquivo main, crie a classe Stone, responsável por receber cada clique do usuário na tela e atualizar a quantidade de “fragmentos”.
-3. A cada clique na tela, o usuário recebe um fragment. Utilize a imagem:
+2. Ainda no arquivo main, crie a classe Stone, 
+responsável por receber cada clique do usuário na tela e 
+atualizar a quantidade de “fragmentos”.
+3. A cada clique na tela, o usuário recebe um fragment. 
+Utilize a imagem:
     ![](https://i.imgur.com/jTqNa7D.png)
-4. Quando alcançar 10 fragments, o usuário recebe 1 piece, zera os fragments e muda a imagem para:
+4. Quando alcançar 10 fragments, o usuário recebe 1 piece, 
+zera os fragments e muda a imagem para:
     ![](https://i.imgur.com/IR5Sn6X.png)
-5. Quando alcançar 10 pieces, o usuário recebe 1 stone, zera os pieces e muda a imagem para:
+5. Quando alcançar 10 pieces, o usuário recebe 1 stone, 
+zera os pieces e muda a imagem para:
     ![](https://i.imgur.com/JAeGRb1.png)
-6. Mostre as atualizações de cada clique e a quantidade de fragments, pieces e stones que o usuário tem.
+6. Mostre as atualizações de cada clique e a quantidade de fragments,
+ pieces e stones que o usuário tem.
 
 ```
 Entrada:
@@ -65,48 +71,79 @@ Imagens:
 ### [Vídeo explicativo](https://drive.google.com/file/d/14j8egTzUq6qTgzAQXk81Be5287aEARpW/view?usp=sharing)
 
 */
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const MyCoin());
+  runApp(const Stone());
 }
 
-class MyCoin extends StatefulWidget {
-  const MyCoin({super.key});
+class Stone extends StatefulWidget {
+  const Stone({super.key});
 
   @override
-  State<MyCoin> createState() => _MyCoinState();
+  State<Stone> createState() => _StoneState();
 }
 
-class _MyCoinState extends State<MyCoin> {
-  num copper = 0;
-  num silver = 0;
-  num gold = 0;
+class _StoneState extends State<Stone> {
+  num fragments = 0;
+  num pieces = 0;
+  num stone = 0;
+  String imagem = "https://i.imgur.com/etWX9CB.jpg";
 
-  void coinCount() {
+  void stoneCount() {
     setState(() {
-      copper++;
-      if (copper == 10) {
-        silver++;
-        copper = 0;
+      fragments++;
+
+      if (fragments >= 10) {
+        pieces++;
+        fragments = 0;
+        //imagem = 'https://i.imgur.com/IR5Sn6X.png';
       }
-      if (silver == 10) {
-        gold++;
-        silver = 0;
+      if (pieces >= 10) {
+        stone++;
+        pieces = 0;
+        //  imagem = 'https://i.imgur.com/JAeGRb1.png';
+      }
+
+      if (fragments >= 1) {
+        imagem = 'https://i.imgur.com/jTqNa7D.png';
+      }
+      if (pieces >= 1) {
+        imagem = 'https://i.imgur.com/IR5Sn6X.png';
+      }
+      if (stone >= 1) {
+        imagem = 'https://i.imgur.com/JAeGRb1.png';
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-        onTap: coinCount,
-        child: Text(
-          'Copper $copper \n Silver $silver\n Gold $gold',
-          textDirection: TextDirection.ltr,
-        ),
+    return MaterialApp(
+      home: Center(
+        child: GestureDetector(
+            onTap: stoneCount,
+            child: Column(children: [
+              Expanded(
+                child: Stack(children: [
+                  Image.network('https://i.imgur.com/etWX9CB.jpg'),
+                  Align(
+                      child: Container(
+                          child: Image.network(imagem),
+                          padding: EdgeInsets.all(10)),
+                      alignment: Alignment.topCenter),
+                  Center(
+                    child: Container(
+                        child: Text(
+                          'Stones $stone\nPieces $pieces\nFragments $fragments',
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.center,
+                        ),
+                        padding: EdgeInsets.all(20)),
+                  ),
+                ]),
+              )
+            ])),
       ),
     );
   }
